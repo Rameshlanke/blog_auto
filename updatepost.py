0f8c16bcfe9ac894f38530d46c5519a9d8775c71
Ramesh Lanke <66162970+Rameshlanke@users.noreply.github.com>
2023-06-21 20:35:20 +0530

    Update updatepost.py

--- updatepost.py
+++ updatepost.py
@@ -6,7 +6,12 @@ from googleapiclient import discovery
 import mammoth
 from base2 import get_courses_html
 import datetime
+from base2 import get_courses_html
+from flask import Flask, request
 
+
+
+app = Flask(__name__)
 
 # Function to build HTML from file
 def build_html(file_path):
@@ -20,14 +25,6 @@ def build_html(file_path):
     else:
         raise ValueError('Invalid file extension. Only .html and .docx files are supported.')
     return html
-
-# Build HTML content from files
-html1 = build_html('first.html')
-html4 = "Updated Time (IST): " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M") + "\n"
-html2 = get_courses_html()
-html3 = build_html('file2.docx')
-all_html = html1 + html4 + html2 + html3
-
 
 
 # Function to authorize credentials
@@ -44,6 +41,8 @@ def authorize_credentials():
 
     return credentials
 
+
+
 # Function to update a blog post
 def update_post(post_id, blog_id, title, content):
     credentials = authorize_credentials()
@@ -58,23 +57,39 @@ def update_post(post_id, blog_id, title, content):
     updated_post = service.posts().update(blogId=blog_id, postId=post_id, body=post).execute()
     print('Post updated successfully!')
 
-# Get the current date
-from datetime import date
-today = date.today().strftime("%d-JUNE-%y")
-title = f"UDEMY COURSES WITH FREE CERTIFICATE | {today} | IHTREEKTECHCOURSES"
 
-# Specify the post ID, blog ID, title, and content to update
-post_id = '8097252119400957177'
-blog_id = '5651255862571344247'
-test_post_id = '3359690114760599976'
-test_blog_id = '3481597162520342949'
-content = all_html
-# Call the update_post function to update the blog post
-update_post(post_id, blog_id, title, content)
-# update_post(test_post_id, test_blog_id, title, content)
 
-# Delete temporary files
-# file_list = ['post.docx', 'modified.docx']
-# for file in file_list:
-#     os.remove(file)
-#     print(f"{file} deleted successfully!")
+
+@app.route('/')
+def index():
+    return 'Welcome to the Flask app!'
+
+
+# @app.route('/update', methods=['POST'])
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    # Specify the post ID, blog ID, title, and content to update
+    post_id = '8097252119400957177'
+    blog_id = '5651255862571344247'
+    test_post_id = '3359690114760599976'
+    test_blog_id = '3481597162520342949'
+
+    # Build HTML content from files
+    html1 = build_html('first.html')
+    html4 = "Updated Time (IST): " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M") + "\n"
+    html2 = get_courses_html()
+    html3 = build_html('file2.docx')
+    all_html = html1 + html4 + html2 + html3
+    # Get the current date
+    from datetime import date
+    today = date.today().strftime("%d-JUNE-%y")
+    title = f"UDEMY COURSES WITH FREE CERTIFICATE | {today} | IHTREEKTECHCOURSES"
+
+
+    content = all_html
+    update_post(post_id, blog_id, title, content)
+    return 'Post updated successfully!'
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
